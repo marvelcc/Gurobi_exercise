df459540427780178032934ad4f25cb9e1c06d48
marvelcc <chaolong.c@gmail.com>
2023-06-11 13:41:11 +0200

    finished exercise

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ facilities = ['j1', 'j2', 'j3']
 
 demand = {'i1': 80, 'i2': 270, 'i3': 250, 'i4': 160, 'i5': 180}
 activation_cost = {'j1': 1000, 'j2': 1000, 'j3': 1000}
-capacity = {'cap_j1': 500, 'cap_j2': 500, 'cap_j3': 500}
+capacity = {'j1': 500, 'j2': 500, 'j3': 500}
 transport_cost = {('i1', 'j1'): 4,
                   ('i1', 'j2'): 6,
                   ('i1', 'j3'): 9,
@@ -26,12 +26,40 @@ transport_cost = {('i1', 'j1'): 4,
                   ('i5', 'j3'): 4}
 
 
-print(transport_cost['i1', 'j3'])
-# # Create model
-# m = gp.Model('FLP')
+# Create model
+m = gp.Model('FLP')
 
 
-# # Create variables
-# m.addVars(vtype='BINARY', name='x')
+# Add variables
+x = m.addVars(customers, facilities, vtype=GRB.INTEGER)
+y = m.addVars(facilities, vtype=GRB.BINARY)
+# Set objective function
+# factory cost (f_j * y_j) + transportation cost (c_ij * x_ij)
+m.setObjective(gp.quicksum(activation_cost[j] * y[j] for j in facilities) + gp.quicksum(
+    transport_cost[i, j] * x[i, j] for i in customers for j in facilities), GRB.MINIMIZE)
+
+# Add constraints
+# sum(x_ij) == d_i
+m.addConstrs(gp.quicksum(x[i, j]
+             for j in facilities) == demand[i] for i in customers)
+
+# sum(x_ij) <= M_j * y_j
+m.addConstrs(gp.quicksum(x[i, j] for i in customers)
+             <= capacity[j] * y[j] for j in facilities)
+
+# # x_ij <= d_i * y_j
+m.addConstrs(x[i, j] <= demand[i] * y[j]
+             for i in customers for j in facilities)
+
+m.optimize()
+
+
+if m.Status == GRB.OPTIMAL:
+    flow = m.getAttr('X', x)
+    open = m.getAttr('X', y)
+    for i in customers:
+        for j in facilities:
+            if flow[i, j] > 0:
+                print(str(j) + " -> " + str(i) + ": " + str(flow[i, j]))
 
 
